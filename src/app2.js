8c45428e6dadc3b515d01d930afe41f6af219090
Khalyutkin Victor <53880888+KhalyutkinVictor@users.noreply.github.com>
2023-02-12 21:28:02 +0300

    refactoring

---
+++ src/app2.js
@@ -0,0 +1,119 @@
+import VRuntime2Factory from "./prototype2.js";
+
+const VRuntime2 = new VRuntime2Factory();
+const Atom2 = VRuntime2.atomFactory();
+const Computed2 = VRuntime2.computedFactory();
+
+const todoState2 = new Atom2({
+  todos: []
+});
+
+class TodoApp2 {
+  
+  computed =  [];
+  
+  render() {
+    const container = document.createElement('div');
+    let todosContainer = (new TodoContainer2([])).render();
+    container.append((new TodoInput2()).render())
+    container.append(todosContainer);
+    this.computed.push(new Computed2(() => {
+      const newTodo = (new TodoContainer2(todoState2.get().todos)).render();
+      todosContainer.replaceWith(newTodo);
+      todosContainer = newTodo;
+    }, false));
+    
+    this.el = container;
+    return this.el;
+  }
+  
+}
+
+class TodoInput2 {
+  
+  render() {
+    const container = document.createElement('div');
+    container.innerHTML = `
+        <input type="text" id="td-inpt2">
+        <button id="td-add2">Добавить</button>
+    `;
+    self.inpt = container.querySelector('#td-inpt2');
+    self.btn = container.querySelector('#td-add2');
+    self.btn.onclick = () => TodoInput2.click();
+    return container;
+  }
+  
+  static click() {
+    let currentState = todoState2.get();
+    currentState.todos = [...(currentState.todos ?? []), {name: self.inpt.value, checked: false, id: Math.floor(Math.random() * 100000000)}];
+    self.inpt.value = null;
+    todoState2.set(currentState);
+  }
+}
+
+class TodoContainer2 {
+  
+  constructor(todos) {
+    this.todos = todos;
+  }
+  
+  render() {
+    const container = document.createElement('div');
+    
+    for (let todo of this.todos) {
+      container.append((new Todo2(todo.name, todo.isChecked, todo.id)).render());
+    }
+    
+    return container;
+  }
+  
+}
+
+class Todo2 {
+  
+  constructor(name, isChecked, id) {
+    this.name = name;
+    this.isChecked = isChecked;
+    this.id = id;
+  }
+  
+  render() {
+    const container = document.createElement('div');
+    container.innerHTML = `
+        <div style="display: flex; gap: 1rem">
+            <input type="checkbox" ${this.isChecked ? 'checked' : ''}>
+            <span>${this.name}</span>
+            <button class="todo-dlt2">X</button>
+        </div>
+    `;
+    container.querySelector('.todo-dlt2').onclick = () => Todo2.remove(this.id);
+    return container;
+  }
+  
+  static remove(id) {
+    const state = todoState2.get();
+    state.todos = state.todos.filter(todo => todo.id !== id);
+    todoState2.set(state);
+  }
+  
+}
+
+let app2 = new TodoApp2();
+
+document.getElementById('app2').append(app2.render());
+
+let vc2 = new Computed2(function() {
+  return todoState2.get().todos.length + 1;
+});
+
+let bt2 = new Computed2(function () {
+  return vc2.get() + 1;
+});
+
+new Computed2(() => {
+  document.getElementById('counter2').innerHTML = bt2.get();
+}, false);
+
+let tasksCount2 = new Computed2(() => todoState2.get().todos.length);
+
+document.getElementById('cnt-btn2').onclick = () => alert(tasksCount2.get());
---
+++ src/prototype2.js
@@ -0,0 +1,191 @@
+// Runtime that can work with async functions
+
+/**
+ * @property {VRuntime} runtime
+ */
+class __VR_ObsGetter {
+  
+  _obsMode = false;
+  
+  exitObsMode() {
+    this._obsMode = false;
+  }
+  
+  get() {
+    if (this.runtime.observableMode) {
+      if (this._obsMode) {
+        throw new Error('Circular dependencies');
+      }
+      this.runtime.addDep(this);
+      this._obsMode = true;
+    }
+  }
+  
+}
+
+/**
+ * @property {VRuntime} runtime
+ */
+class __VR_Reactive extends __VR_ObsGetter {
+  
+  pubs = [];
+  subs = [];
+  
+  addSub(reactive) {
+    if (!this.subs.includes(reactive)) {
+      this.subs.push(reactive);
+    }
+  }
+  
+  addPub(reactive) {
+    if (!this.pubs.includes(reactive)) {
+      this.pubs.push(reactive);
+    }
+  }
+  
+}
+
+/**
+ * @property {VRuntime} runtime
+ */
+class __VR_Atom extends __VR_Reactive {
+  
+  _val = undefined;
+  
+  constructor(val) {
+    super();
+    this._val = val;
+  }
+  
+  set(val) {
+    this._val = val;
+    this.runtime.startReaction(this);
+  }
+  
+  get() {
+    super.get();
+    return this._val;
+  }
+
+}
+
+/**
+ * @property {VRuntime} runtime
+ */
+class __VR_Computed extends __VR_Reactive {
+
+  _fn = undefined;
+  _val = undefined;
+  isCacheValid = false;
+  
+  constructor(fn, lazy = true) {
+    super();
+    this.lazy = lazy;
+    this._fn = fn;
+    this.runtime.registerComputed(this);
+  }
+
+  get() {
+    super.get();
+    return this.isCacheValid
+      ? this._val
+      : this.calcActualValue();
+  }
+  
+  calcActualValue() {
+    this._val = this._fn();
+    this.isCacheValid = true;
+    return this._val;
+  }
+  
+  pull() {
+    if (!this.lazy) {
+      this.calcActualValue();
+      return;
+    }
+    this.isCacheValid = false;
+    this.runtime.reactivePull(this);
+  }
+  
+  push() {
+    this.calcActualValue();
+    for (let comp of this.subs) {
+      this.runtime.reactivePush(comp);
+    }
+  }
+  
+}
+
+class VRuntime {
+
+  _atomClass = undefined;
+  _computedClass = undefined;
+  
+  observableMode = false;
+  
+  obsList = [];
+  
+  /**
+   * @param {{atomClass: Function, computedClass: Function}} config
+   */
+  constructor(config = {}) {
+    config = {
+      atomClass: __VR_Atom,
+      computedClass: __VR_Computed,
+      ...config
+    };
+    this._atomClass = config.atomClass;
+    this._computedClass = config.computedClass;
+  }
+  
+  registerComputed(comp) {
+    this.observableMode = true;
+    this.obsList = [];
+    comp.calcActualValue();
+    for (let reactive of this.obsList) {
+      reactive.exitObsMode();
+      reactive.addSub(comp);
+      comp.addPub(reactive);
+    }
+    this.observableMode = false;
+  }
+  
+  addDep(obs) {
+    this.obsList.push(obs);
+  }
+  
+  atomFactory() {
+    if (!this._atomClass) {
+      throw new Error('Atom class is not defined');
+    }
+    this._atomClass.prototype.runtime = this;
+    return this._atomClass;
+  }
+
+  computedFactory() {
+    if (!this._computedClass) {
+      throw new Error('Computed class is not defined');
+    }
+    this._computedClass.prototype.runtime = this;
+    return this._computedClass;
+  }
+  
+  reactivePull(reactive) {
+    for (let computed of reactive.subs) {
+      computed.pull();
+    }
+  }
+  
+  reactivePush(reactive) {
+    for (let computed of reactive.subs) {
+      computed.push();
+    }
+  }
+  
+  startReaction(atom) {
+    this.reactivePull(atom);
+  }
+  
+}
+
+export default VRuntime;
